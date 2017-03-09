5509d1d687cdf3b086475016f0b97d542a028665
geeebro <djmax256081@163.com>
2017-03-09 11:03:35 +0800

    remove commit

--- tasks/uglify.js
+++ tasks/uglify.js
@@ -1,9 +1,9 @@
 module.exports = {
   build: {
-    options: {
-      preserveComments: 'some'
-      // ,sourceMap: true
-    },
+    // options: {
+    //   preserveComments: 'some'
+    //   // ,sourceMap: true
+    // },
     files: {
       'dist/index.min.js': ['dist/index.js']
     }
